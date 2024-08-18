54fd2b4c513c10a21a8b8248e45257011f78e27f
René Lebherz <me@renelebherz.com>
2024-08-18 16:07:16 +0200

    chore: add gradle build files

---
+++ build.gradle.kts
@@ -0,0 +1,10 @@
+plugins {
+	id("buildlogic.java-library-conventions")
+	id("buildlogic.swt-library-conventions")
+	id("buildlogic.artifact-conventions")
+	id("buildlogic.version-conventions")
+}
+
+dependencies {
+	implementation("com.fasterxml.jackson.core:jackson-databind:2.17.2")
+}
